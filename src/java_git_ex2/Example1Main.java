bea59361267be10b887d3875ca64b685f38801cf
UJUNS <tjr4756@naver.com>
2023-03-14 11:05:44 +0900

    second commit

---
+++ src/java_git_ex2/Example1Main.java
@@ -0,0 +1,13 @@
+package java_git_ex2;
+
+public class Example1Main {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Example2 example2 = new Example2();
+		example2.oop();
+		
+	}
+
+}
--- src/java_git_ex2/Example2.java
+++ src/java_git_ex2/Example2.java
@@ -1,10 +1,7 @@
 package java_git_ex2;
 
-public class Example1 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Example2 {
+	public void oop() {
 		for (int i = 2; i < 10; i++) {
 			System.out.println(i + "단");
 
@@ -13,7 +10,6 @@ public class Example1 {
 			}
 			System.out.println();
 		}
-
 	}
 
 }
